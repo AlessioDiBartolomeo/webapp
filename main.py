fc68ef23c8bee76b05b310c8a145733e77d88553
AlessioDiBartolomeo <alessio.dibartolomeo19@gmail.com>
2024-03-11 11:24:44 +0100

    aggiunta image/heic

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ with st.sidebar:
     st.header("Image Config")     # Adding header to sidebar
     # Adding file uploader to sidebar for selecting images
     source_img = st.file_uploader(
-        "Upload an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp', 'heic'))
+        "Upload an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp', 'heic', 'image/heic'))
  
     # Model Options
     confidence = float(st.slider(
